6afabe58119f99471b73f9f9c9fc18bf25826dc1
Azarattum <Azarattum@pm.me>
2019-02-13 21:27:37 +0700

    Implemented cart service

---
+++ services/CartSvc.js
@@ -0,0 +1,49 @@
+/*
+** Cart service for the shop
+*/
+app.factory("Cart", [() => {
+	let cart = [];
+
+	let exists = (id) => {
+		//Check if there is any item with provided id
+		return cart.some(x => x.id == id);
+	};
+
+	let add = (item, count = 1) => {
+		//Add new item
+		if (!exists(item.id))
+		{
+			item.count = count;
+			cart.push(item);
+		}
+		//Increase existing item
+		else
+			cart.find(x => x.id == item.id).count += count;
+	};
+
+	let remove = (id, count = 1) => {
+		cart = cart.filter(x => {
+			//Find item by id
+			if (x.id == id)
+			{
+				//Decrease count of the item
+				x.count -= count;
+				if (x.count <= 0)
+					return false;
+			}
+			return true;
+		});
+	};
+
+	let get = () => {
+		//Returns the cart
+		return cart;
+	};
+
+	return {
+		exists: exists,
+		add: add,
+		remove: remove,
+		get: get
+	}
+}]);
